8382f84f6c7260d90f5e932ae9ea142916472c2c
Ruslan Shishmarev <mr.shruslan@gmail.com>
2024-09-25 22:54:05 +0300

    pep8

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from users.models import Follow, FoodgramUser
 
+
 @admin.register(FoodgramUser)
 class FoodgramUserAdmin(admin.ModelAdmin):
     """Создание объекта пользователя в админ панели."""
@@ -14,6 +15,7 @@ class FoodgramUserAdmin(admin.ModelAdmin):
     list_filter = ('email', 'username')
     search_fields = ('email', 'username')
     empty_value_display = 'Поле не заполнено'
+
 
 @admin.register(Follow)
 class SubscriptionAdmin(admin.ModelAdmin):
