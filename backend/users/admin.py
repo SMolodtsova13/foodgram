64985cd79b3770d1c4599b95481281f59773aca8
Ruslan Shishmarev <mr.shruslan@gmail.com>
2024-09-25 22:51:27 +0300

    pep8

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-# from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
 
 from users.models import Follow, FoodgramUser
-
-# User = get_user_model()
 
 @admin.register(FoodgramUser)
 class FoodgramUserAdmin(admin.ModelAdmin):
@@ -18,23 +14,6 @@ class FoodgramUserAdmin(admin.ModelAdmin):
     list_filter = ('email', 'username')
     search_fields = ('email', 'username')
     empty_value_display = 'Поле не заполнено'
-
-# UserAdmin.fieldsets += (
-#     ('Extra Fields', {'fields': ('avatar',
-#                                  'role',
-#                                  )}),
-# )
-# UserAdmin.list_display += (
-#     'avatar',
-#     'role',
-# )
-# UserAdmin.search_fields = ('email', 'username')
-# UserAdmin.verbose_name = 'Пользователь'
-# UserAdmin.actions += ('change_selected',
-#                       'delete_selected')
-# UserAdmin.empty_value_display = 'Поле не заполнено'
-# admin.site.register(FoodgramUser, UserAdmin)
-
 
 @admin.register(Follow)
 class SubscriptionAdmin(admin.ModelAdmin):
