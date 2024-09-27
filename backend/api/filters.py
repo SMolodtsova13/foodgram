dc1421084099aec010fc496b8c0c5bd6e1a4ed23
Ruslan Shishmarev <mr.shruslan@gmail.com>
2024-09-27 21:00:33 +0300

    update filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from django_filters import filters
-from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework import FilterSet, filters
 
 from recipes.models import Recipe, Tag
 
@@ -14,25 +13,26 @@ class RecipeFilter(FilterSet):
         field_name='tags__slug',
         to_field_name='slug'
     )
-    is_favorited = filters.NumberFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.NumberFilter(
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
         method='filter_in_shopping_list'
     )
 
     def filter_is_favorited(self, queryset, name, value):
-        if value == 1:
+        if value:
             user = self.request.user
-            print(str(queryset))
-            return queryset.filter(favorites__user_id=user.id)
+            if user.is_authenticated:
+                return queryset.filter(favorites__user_id=user.id)
         return queryset
 
     def filter_in_shopping_list(self, queryset, name, value):
-        if value == 1:
+        if value:
             user = self.request.user
-            print(str(queryset))
-            return queryset.filter(shopping_recipe__user_id=user.id)
+            if user.is_authenticated:
+                return queryset.filter(shopping_recipe__user_id=user.id)
         return queryset
 
     class Meta:
         model = Recipe
         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
