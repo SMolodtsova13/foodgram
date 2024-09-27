0060e59525c8a5c8c7ee83343aa9bae388f693a3
Ruslan Shishmarev <mr.shruslan@gmail.com>
2024-09-27 21:21:48 +0300

    ppp

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,7 +7,8 @@ from api.views import (IngredientViewSet, TagViewSet,
 app_name = 'api'
 
 router = routers.DefaultRouter()
-router.register(r'tags', TagViewSet, basename='tags')
+
+router.register('tags', TagViewSet, basename='tags')
 router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 router.register(r'users', FoodgramUserViewSet, basename='users')
