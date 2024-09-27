ba0280e8ada540b709a2503784af0cc0a22b2cc3
Ruslan Shishmarev <mr.shruslan@gmail.com>
2024-09-27 21:19:40 +0300

    pep

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,12 +7,12 @@ from api.views import (IngredientViewSet, TagViewSet,
 app_name = 'api'
 
 router = routers.DefaultRouter()
+router.register(r'tags', TagViewSet, basename='tags')
 router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 router.register(r'users', FoodgramUserViewSet, basename='users')
-router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path('', include(router.urls)),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
-]# path('auth/', include('djoser.urls')),
+]  # path('auth/', include('djoser.urls')),
