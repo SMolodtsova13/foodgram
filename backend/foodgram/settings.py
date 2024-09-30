144e1ab4e4967bd9ff6eff5c3f894783dd830b17
Ruslan Shishmarev <mr.shruslan@gmail.com>
2024-10-01 00:55:47 +0300

    настройки автоматизации

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,19 +1,16 @@
 import os
+from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 PAGE_SIZE = 6
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
-
-if not SECRET_KEY:
-    SECRET_KEY = get_random_secret_key()
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
@@ -115,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = "/media/"
 
